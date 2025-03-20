c9974852bd0da7f815b4212de182aa725fe4690b
Gabriel Bovi Lorandi <gabriel.bovi96@gmail.com>
2025-03-20 13:22:31 -0500

    refactor(engine): replace file-based query execution with string-based execution

--- parse.go
+++ parse.go
@@ -2,21 +2,12 @@ package engine
 
 import (
 	"bytes"
-	"os"
 )
 
-func Execute(name string, args any) (string, []any) {
+func Execute(query string, args any) (string, []any) {
 	engine := NewEngine()
-	buf := new(bytes.Buffer)
 
-	file, err := os.Open(name)
-	if err != nil {
-		panic(err)
-	}
-
-	file.WriteTo(buf)
-
-	tmpl, err := engine.tempalte.Parse(buf.String())
+	tmpl, err := engine.tempalte.Parse(query)
 	if err != nil {
 		panic(err)
 	}
